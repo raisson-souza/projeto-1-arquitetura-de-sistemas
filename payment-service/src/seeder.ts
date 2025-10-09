c3f7ba1ef1bbb4d979e5104e2af9be6d3ae5f09f
Raisson Souza <raissonrai@gmail.com>
2025-10-09 11:03:47 -0300

    Implementada validação de seeders já semeados.

--- payment-service/src/seeder.ts
+++ payment-service/src/seeder.ts
@@ -6,90 +6,102 @@ dotenv.config()
 const prisma = new PrismaClient()
 
 const seedDb = async () => {
-    await prisma.paymentOrderStatus.createMany({
-        data: [
-            {
-                id: 1,
-                description: "Em Aberto",
-            },
-            {
-                id: 2,
-                description: "Finalizado",
-            },
-            {
-                id: 3,
-                description: "Cancelado",
-            },
-            {
-                id: 4,
-                description: "Reembolsado",
-            },
-        ]
-    })
-        .then(() => console.log("Status de Pedidos de Pagamento criados."))
-        .catch(ex => console.log(`Erro em status de pedidos de pagamento. Erro: ${ (ex as Error).message }`))
+    const seedOrderStatus = (await prisma.paymentOrderStatus.findMany()).length === 0
 
-    await prisma.paymentStatus.createMany({
-        data: [
-            {
-                id: 1,
-                description: "Em Aberto",
-            },
-            {
-                id: 2,
-                description: "Finalizado",
-            },
-            {
-                id: 3,
-                description: "Cancelado",
-            },
-            {
-                id: 4,
-                description: "Reembolsado",
-            },
-            {
-                id: 5,
-                description: "Aguardando Confirmação Externa",
-            },
-            {
-                id: 6,
-                description: "Erro",
-            },
-            {
-                id: 7,
-                description: "Recusado",
-            },
-        ]
-    })
-        .then(() => console.log("Status de Pagamentos criados."))
-        .catch(ex => console.log(`Erro em status de pagamentos. Erro: ${ (ex as Error).message }`))
+    if (seedOrderStatus) {
+        await prisma.paymentOrderStatus.createMany({
+            data: [
+                {
+                    id: 1,
+                    description: "Em Aberto",
+                },
+                {
+                    id: 2,
+                    description: "Finalizado",
+                },
+                {
+                    id: 3,
+                    description: "Cancelado",
+                },
+                {
+                    id: 4,
+                    description: "Reembolsado",
+                },
+            ]
+        })
+            .then(() => console.log("Status de Pedidos de Pagamento criados."))
+            .catch(ex => console.log(`Erro em status de pedidos de pagamento. Erro: ${ (ex as Error).message }`))
+    }
 
-    await prisma.paymentMethod.createMany({
-        data: [
-            {
-                id: 1,
-                description: "Dinheiro",
-            },
-            {
-                id: 2,
-                description: "Cartão de Débito",
-            },
-            {
-                id: 3,
-                description: "Cartão de Crédito",
-            },
-            {
-                id: 4,
-                description: "Boleto",
-            },
-            {
-                id: 5,
-                description: "Pix",
-            },
-        ]
-    })
-        .then(() => console.log("Métodos de pagamento criados."))
-        .catch(ex => console.log(`Erro em métodos de pagamento. Erro: ${ (ex as Error).message }`))
+    const seedPaymentStatus = (await prisma.paymentStatus.findMany()).length === 0
+
+    if (seedPaymentStatus) {
+        await prisma.paymentStatus.createMany({
+            data: [
+                {
+                    id: 1,
+                    description: "Em Aberto",
+                },
+                {
+                    id: 2,
+                    description: "Finalizado",
+                },
+                {
+                    id: 3,
+                    description: "Cancelado",
+                },
+                {
+                    id: 4,
+                    description: "Reembolsado",
+                },
+                {
+                    id: 5,
+                    description: "Aguardando Confirmação Externa",
+                },
+                {
+                    id: 6,
+                    description: "Erro",
+                },
+                {
+                    id: 7,
+                    description: "Recusado",
+                },
+            ]
+        })
+            .then(() => console.log("Status de Pagamentos criados."))
+            .catch(ex => console.log(`Erro em status de pagamentos. Erro: ${ (ex as Error).message }`))
+    }
+
+    const seedPaymentMethods = (await prisma.paymentMethod.findMany()).length === 0
+
+    if (seedPaymentMethods) {
+        await prisma.paymentMethod.createMany({
+            data: [
+                {
+                    id: 1,
+                    description: "Dinheiro",
+                },
+                {
+                    id: 2,
+                    description: "Cartão de Débito",
+                },
+                {
+                    id: 3,
+                    description: "Cartão de Crédito",
+                },
+                {
+                    id: 4,
+                    description: "Boleto",
+                },
+                {
+                    id: 5,
+                    description: "Pix",
+                },
+            ]
+        })
+            .then(() => console.log("Métodos de pagamento criados."))
+            .catch(ex => console.log(`Erro em métodos de pagamento. Erro: ${ (ex as Error).message }`))
+    }
 }
 
 seedDb()
--- user-service/src/seeder.ts
+++ user-service/src/seeder.ts
@@ -6,37 +6,45 @@ dotenv.config();
 const prisma = new PrismaClient()
 
 const seedDb = async () => {
-    await prisma.userType.createMany({
-        data: [
-            {
-                id: 1,
-                description: "administrador",
-            },
-            {
-                id: 2,
-                description: "client",
-            },
-            {
-                id: 3,
-                description: "vendedor",
-            },
-        ]
-    })
-        .then(() => console.log("Tipos de usuários criados."))
-        .catch(ex => console.log(`Erro em tipos de usuários. Erro: ${ (ex as Error).message }`))
+    const seedUserTypes = (await prisma.userType.findMany()).length === 0
 
-    await prisma.user.createMany({
-        data: [
-            {
-                id: 1,
-                name: "Administrador",
-                email: "administrador@gmail.com",
-                typeId: 1,
-            },
-        ]
-    })
-        .then(() => console.log("Usuários criados."))
-        .catch(ex => console.log(`Erro em usuários. Erro: ${ (ex as Error).message }`))
+    if (seedUserTypes) {
+        await prisma.userType.createMany({
+            data: [
+                {
+                    id: 1,
+                    description: "administrador",
+                },
+                {
+                    id: 2,
+                    description: "client",
+                },
+                {
+                    id: 3,
+                    description: "vendedor",
+                },
+            ]
+        })
+            .then(() => console.log("Tipos de usuários criados."))
+            .catch(ex => console.log(`Erro em tipos de usuários. Erro: ${ (ex as Error).message }`))
+    }
+
+    const seedUsers = (await prisma.user.findMany()).length === 0
+
+    if (seedUsers) {
+        await prisma.user.createMany({
+            data: [
+                {
+                    id: 1,
+                    name: "Administrador",
+                    email: "administrador@gmail.com",
+                    typeId: 1,
+                },
+            ]
+        })
+            .then(() => console.log("Usuários criados."))
+            .catch(ex => console.log(`Erro em usuários. Erro: ${ (ex as Error).message }`))
+    }
 }
 
 seedDb()
